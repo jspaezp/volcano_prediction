50116442a6003e3862aeeedbd0acd3da7a0e6a76
Jspaezp <jspaezp@gmail.com>
2020-11-19 00:05:53 -0500

    added evaluation to the training (no benchmars are output though ...)

--- train.py
+++ train.py
@@ -5,6 +5,9 @@ from pathlib import Path
 import pandas as pd
 from tqdm import tqdm
 
+import torch.optim as optim
+import torch.nn as nn
+from v_models import resnet_10r
 
 class tensorLoader(Dataset):
     def __init__(self, train_df, filepath):
@@ -15,7 +18,8 @@ class tensorLoader(Dataset):
         for i, (x, y) in enumerate(my_iter):
             db_map.update({x: i})
             x = str(x)
-            y = torch.tensor(math.log10(float(y)))
+            y = [math.log10(float(y))]
+            y = torch.tensor(y)
             db.append({"path": (Path(filepath) / f"{x}.pt"), "value": y})
 
         self.db = db
@@ -37,27 +41,47 @@ class tensorLoader(Dataset):
         return data_tensor[0,:,:,:], item["value"]
 
 
-if __name__ == "__main__":
-    import torch.optim as optim
-    import torch.nn as nn
-    from v_models import resnet_10r
+def evaluate(net, testloader):
+    expected = []
+    predicted = []
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            predicted.append(outputs)
+            expected.append(labels)
 
+    return expected, predicted
+            
+
+def main(train_file = "train.csv", data_path = Path("./train-tensors"), epochs = 2):
     net = resnet_10r()
     criterion = nn.MSELoss()
     optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-    train_df = pd.read_csv("train.csv")
+    df = pd.read_csv(train_file)
+    df_copy = df.copy()
+    train_set = df.sample(frac=0.95, random_state=0)
+    test_set = df_copy.drop(train_set.index)
+
     # .glob("*.pt")
-    traindata = tensorLoader(train_df, Path("./train-tensors"))
+    traindata = tensorLoader(train_set, data_path)
     trainloader = torch.utils.data.DataLoader(
-        traindata, batch_size=4, shuffle=True, num_workers=2
+        traindata, batch_size=1, shuffle=True, num_workers=5
     )
 
-    for epoch in range(2):  # loop over the dataset multiple times
+    testdata = tensorLoader(test_set, data_path)
+    testloader = torch.utils.data.DataLoader(
+        testdata, batch_size=1, shuffle=True, num_workers=5
+    )
+
+    for epoch in range(epochs):  # loop over the dataset multiple times
 
         running_loss = 0.0
         tot_running_loss = 0.0
+        curr_running_loss = 0.0
         prog_bar = tqdm(trainloader)
+
         for i, data in enumerate(prog_bar, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
@@ -78,10 +102,19 @@ if __name__ == "__main__":
 
             curr_runn_loss = running_loss / (i + 1)
 
-            prog_bar.set_postfix({'running_loss': curr_runn_loss})
+            prog_bar.set_postfix({'epoch_loss': curr_runn_loss, })
 
-            if i % 200 == 199:  # print every 200 mini-batches
-                print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 200))
+            if i % 100 == 99:  # print every 200 mini-batches
+                curr_running_loss = running_loss / 100
+                print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, curr_running_loss))
                 running_loss = 0.0
 
+        expected, predicted = evaluate(nn, testloader)
+
     print("Finished Training")
+    return net
+
+if __name__ == "__main__":
+
+    main()
+
