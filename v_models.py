f4f1a47e16df0673c8099bdb72dbf836f5c4c897
Jspaezp <jspaezp@gmail.com>
2020-11-24 06:50:18 -0500

    added some testing and fixed visualization

--- v_models.py
+++ v_models.py
@@ -75,47 +75,22 @@ def test_resnet_10r():
     x_image = torch.randn(1, 10, 224, 224)
     for _ in range(20):
         model = resnet_10r()
-        ouput = model(x_image)
-        print(ouput)
-    # print(model.parameters)
+        output = model(x_image)
+        print(output)
+
+    model = resnet_10r()
+    output = model(x_image)
+    assert len(output) == 1
 
 
-#
-class MyModel(nn.Module):
-    def __init__(self):
-        super(MyModel, self).__init__()
-        image_modules = list(resnet_10c().children())[
-            :-1
-        ]  # all layer expect last layer
-        self.modelA = nn.Sequential(*image_modules)
-
-    def forward(self, image):
-        a = self.modelA(image)
-        x = nnf.log_softmax(a)
-        return x
-
-
-if __name__ == "__main__":
-
+def test_resnet_10c():
+    resnet_10c(layers = [2,3,4,5])
     x_image = torch.randn(1, 10, 224, 224)
-
-    model = MyModel()
-    model.fc = nn.Linear(2048, 1)
-
-    print(">>>>>>> Out for MyModel, replacing last layer")
-    ouput = model(x_image)
-    print(ouput.shape)
-
-    print(">>>>>>> Out for resnet 10c")
     model = resnet_10c()
     ouput = model(x_image)
     print(ouput)
 
-    print(">>>>>>> Out for resnet 10r")
-    for _ in range(20):
-        model = resnet_10r()
-        ouput = model(x_image)
-        print(ouput)
-    # print(model.parameters)
 
-    print(">>>>>>> Out for resnet 10r")
+if __name__ == "__main__":
+    pass
+
--- visualize.py
+++ visualize.py
@@ -1,13 +1,24 @@
 import torch
-import cv2
 import matplotlib.pyplot as plt
 
 
 def show_tensor(a):
-    assert len(a.shape) == 3
+    if len(a.shape) != 3:
+        print(f"Provided tensor is not 3d {a.shape}, will try to make it such ...")
+        a = a[0,:,:,:]
+
+    if a.shape[0] > 100:
+        print("Probably you did not want to pass this array ...")
     # Tensor of shape (channels, w, h)
-    ret_a = list(torch.split(a, 1, 0))
-    ret_a = [x.cpu().numpy() for x in ret_a]
-    full = cv2.vconcat(ret_a)
-    plt.imshow(-full)
+    linear_tensor = torch.cat([a[x, :, :] for x in range(a.shape[0])],1) 
+    plt.imshow(-linear_tensor)
     plt.show()
+
+def test_show_tensor():
+    a = torch.rand((10, 50, 50))
+    show_tensor(a)
+    a = torch.rand((1, 10, 50, 50))
+    show_tensor(a)
+
+if __name__ == "__main__":
+    test_show_tensor()
